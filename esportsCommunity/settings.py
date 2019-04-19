9b24a7c1fab3bdd213406f712d490a908762a129
lee southwart <leesouthwart@gmail.com>
2019-04-19 15:12:55 +0000

    allowed host updated

--- esportsCommunity/settings.py
+++ esportsCommunity/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['final-project-leesouthwart.c9users.io']
+ALLOWED_HOSTS = ['final-project-leesouthwart.c9users.io', 'esports-community.herokuapp.com']
 
 
 # Application definition
